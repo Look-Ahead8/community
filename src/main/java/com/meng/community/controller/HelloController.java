bcdc8749f3425576a344671ef3f886006502a172
Look-Ahead8 <1335189318@qq.com>
2019-07-24 16:50:55 +0800

    init rep

---
+++ src/main/java/com/meng/community/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.meng.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Meng
+ * @date 2019/7/24
+ */
+@Controller
+public class HelloController {
+
+}
